32b77a3d91776f9ce91e3333dd1cac669d9c7e73
Learner-Upol <imtryingtoknowmore@gmail.com>
2020-01-15 16:48:43 +0600

    time&date pic main xml update 15.1.20

--- app/src/main/java/com/example/openvotetest/MainActivity.java
+++ app/src/main/java/com/example/openvotetest/MainActivity.java
@@ -2,13 +2,83 @@ package com.example.openvotetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView datePic,timePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        datePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dateIn();
+            }
+        });
+
+        timePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timeCu();
+            }
+        });
     }
+
+    private void timeCu() {
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+
+            }
+        };
+
+        Calendar mTime = Calendar.getInstance();
+        int hour = mTime.get(Calendar.HOUR_OF_DAY);
+        int minute = mTime.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,onTimeSetListener,hour,minute,true);
+        timePickerDialog.show();
+    }
+
+
+    private void dateIn() {
+
+        DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+
+            }
+        };
+
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,onDateSetListener,year,month,day);
+        datePickerDialog.show();
+    }
+
+    private void init() {
+        datePic = findViewById(R.id.dateTv);
+        timePic = findViewById(R.id.timeTv);
+    }
+
+
+
 }
+
